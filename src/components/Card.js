6b1a4659d87c328c62c115fc96c84d5d1c083833
Pavel Zharov <pavel@prna.ru>
2021-08-04 19:35:43 +0700

    feat: use context in Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,34 @@
+import React from "react";
+import { currentUserContext } from "../contexts/CurrentUserContext";
+
 function Card(props) {
+  const card = props.card;
+
+  const currentUser = React.useContext(currentUserContext);
+
+  const isOwn = card.owner._id === currentUser._id;
+  const cardDeleteButtonClassName = (
+    `card__remove-button ${isOwn ? 'card__remove-button_visible' : 'card__remove-button_hidden'}`
+  );
+
+  const isLiked = card.likes.some(i => i._id === currentUser._id);
+  const cardLikeButtonClassName = `${isLiked ? 'like__button_active' : 'like__button_inactive'}`;
 
   const handleOnClick = () => {
-    props.onCardClick(props.card);
+    props.onCardClick(card);
   }
 
   return (
     <li className="elements__list-item">
       <article className="element">
         <div className="element__square-container">
-          <img src={props.card.link} alt={props.card.name} className="element__image" onClick={handleOnClick}/>
+          <img src={card.link} alt={card.name} className="element__image" onClick={handleOnClick}/>
           <button className="element__remove-button button"/>
         </div>
-        <h2 className="element__title">{props.card.name}</h2>
+        <h2 className="element__title">{card.name}</h2>
         <div className="like">
           <button type="button" className="like__button button"/>
-          <span className="like__counter">{props.card.likes.length}</span>
+          <span className="like__counter">{card.likes.length}</span>
         </div>
       </article>
     </li>
