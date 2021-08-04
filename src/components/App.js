11ec92f978ce5f11dc0a114b31ae4a1738763685
Pavel Zharov <pavel@prna.ru>
2021-08-04 18:48:28 +0700

    feat: create current user context

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import PopupWithForm from "./PopupWithForm";
 import ImagePopup from "./ImagePopup";
 import {useEffect, useState} from "react";
 import { api } from "../utils/api";
+import { currentUserContext } from "../contexts/CurrentUserContext";
 
 function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
@@ -43,69 +44,69 @@ function App() {
 
   return (
     <div className="page">
+      <currentUserContext.Provider value={currentUser}>
+        <Header />
+        <Main
+          onEditProfile={handleEditProfileClick}
+          onAddPlace={handleAddPlaceClick}
+          onEditAvatar={handleEditAvatarClick}
+          onCardClick={handleCardClick}
+        />
+        <Footer />
 
-      <Header />
-      <Main
-        onEditProfile={handleEditProfileClick}
-        onAddPlace={handleAddPlaceClick}
-        onEditAvatar={handleEditAvatarClick}
-        onCardClick={handleCardClick}
-      />
-      <Footer />
+        <PopupWithForm
+          name={'edit-profile'}
+          title={'Редактировать профиль'}
+          buttonText={'Сохранить'}
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}>
+          <label htmlFor="name-input" className="form__label">
+            <input type="text" name="name" id="name-input" placeholder="Имя" className="form__input" minLength="2" maxLength="40"
+                  required />
+            <span className="form__input-error form__input-error_visible name-input-error"></span>
+          </label>
+          <label htmlFor="about-input" className="form__label">
+            <input type="text" name="about" id="about-input" placeholder="Занятие" className="form__input" minLength="2" maxLength="200"
+                  required />
+            <span className="form__input-error form__input-error_visible about-input-error"></span>
+          </label>
+        </PopupWithForm>
 
-      <PopupWithForm
-        name={'edit-profile'}
-        title={'Редактировать профиль'}
-        buttonText={'Сохранить'}
-        isOpen={isEditProfilePopupOpen}
-        onClose={closeAllPopups}>
-        <label htmlFor="name-input" className="form__label">
-          <input type="text" name="name" id="name-input" placeholder="Имя" className="form__input" minLength="2" maxLength="40"
-                 required />
-          <span className="form__input-error form__input-error_visible name-input-error"></span>
-        </label>
-        <label htmlFor="about-input" className="form__label">
-          <input type="text" name="about" id="about-input" placeholder="Занятие" className="form__input" minLength="2" maxLength="200"
-                 required />
-          <span className="form__input-error form__input-error_visible about-input-error"></span>
-        </label>
-      </PopupWithForm>
+        <PopupWithForm
+          name={'avatar'}
+          title={'Обновить аватар'}
+          buttonText={'Изменить'}
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}>
+          <label htmlFor="avatar-link-input" className="form__label">
+            <input type="url" name="avatar" id="avatar-link-input" placeholder="Ссылка на картинку"
+                  className="form__input" required />
+            <span className="form__input-error form__input-error_visible avatar-link-input-error"></span>
+          </label>
+        </PopupWithForm>
 
-      <PopupWithForm
-        name={'avatar'}
-        title={'Обновить аватар'}
-        buttonText={'Изменить'}
-        isOpen={isEditAvatarPopupOpen}
-        onClose={closeAllPopups}>
-        <label htmlFor="avatar-link-input" className="form__label">
-          <input type="url" name="avatar" id="avatar-link-input" placeholder="Ссылка на картинку"
-                 className="form__input" required />
-          <span className="form__input-error form__input-error_visible avatar-link-input-error"></span>
-        </label>
-      </PopupWithForm>
+        <PopupWithForm
+          name={'add-card'}
+          title={'Новое место'}
+          buttonText={'Добавить'}
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}>
+          <label htmlFor="place-name-input" className="form__label">
+            <input type="text" name="name" id="place-name-input" placeholder="Название" className="form__input"
+                  minLength="2" required />
+            <span className="form__input-error form__input-error_visible place-name-input-error"></span>
+          </label>
+          <label htmlFor="place-link-input" className="form__label">
+            <input type="url" name="link" id="place-link-input" placeholder="Ссылка на картинку"
+                  className="form__input" required />
+            <span className="form__input-error form__input-error_visible place-link-input-error"></span>
+          </label>
+        </PopupWithForm>
 
-      <PopupWithForm
-        name={'add-card'}
-        title={'Новое место'}
-        buttonText={'Добавить'}
-        isOpen={isAddPlacePopupOpen}
-        onClose={closeAllPopups}>
-        <label htmlFor="place-name-input" className="form__label">
-          <input type="text" name="name" id="place-name-input" placeholder="Название" className="form__input"
-                 minLength="2" required />
-          <span className="form__input-error form__input-error_visible place-name-input-error"></span>
-        </label>
-        <label htmlFor="place-link-input" className="form__label">
-          <input type="url" name="link" id="place-link-input" placeholder="Ссылка на картинку"
-                 className="form__input" required />
-          <span className="form__input-error form__input-error_visible place-link-input-error"></span>
-        </label>
-      </PopupWithForm>
+        <PopupWithForm name='confirmation' title={'Вы уверены?'} buttonText={'Да'} onClose={closeAllPopups}/>
 
-      <PopupWithForm name='confirmation' title={'Вы уверены?'} buttonText={'Да'} onClose={closeAllPopups}/>
-
-      <ImagePopup card={selectedCard} onClose={closeAllPopups} />
-
+        <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+      </currentUserContext.Provider>
     </div>
   );
 }
