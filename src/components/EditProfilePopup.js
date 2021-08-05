ead6ab39de52b4dc0622159faf3c1e984fa8fcc7
Pavel Zharov <pavel@prna.ru>
2021-08-05 19:32:53 +0700

    feat: :sparkles: add managed components to EditProfilePopup

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import PopupWithForm from "./PopupWithForm";
 
 export default function EditProfilePopup (props) {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleNameChange = (e) => {
+      setName(e.target.value);
+    }
+
+    const handleAboutChange = (e) => {
+      setDescription(e.target.value);
+    }
+
     return (
         <PopupWithForm
           name={'edit-profile'}
@@ -9,13 +21,13 @@ export default function EditProfilePopup (props) {
           isOpen={props.isOpen}
           onClose={props.onClose}>
           <label htmlFor="name-input" className="form__label">
-            <input type="text" name="name" id="name-input" placeholder="Имя" className="form__input" minLength="2" maxLength="40"
-                  required />
+            <input type="text" name="name" value={name} id="name-input" placeholder="Имя" className="form__input" minLength="2" maxLength="40"
+                  required onChange={handleNameChange} />
             <span className="form__input-error form__input-error_visible name-input-error"></span>
           </label>
           <label htmlFor="about-input" className="form__label">
-            <input type="text" name="about" id="about-input" placeholder="Занятие" className="form__input" minLength="2" maxLength="200"
-                  required />
+            <input type="text" name="about" value={description} id="about-input" placeholder="Занятие" className="form__input" minLength="2" maxLength="200"
+                  required onChange={handleAboutChange} />
             <span className="form__input-error form__input-error_visible about-input-error"></span>
           </label>
         </PopupWithForm>
