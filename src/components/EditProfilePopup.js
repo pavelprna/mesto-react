367be25e228a0f0000f0e5fcfc26a824c397310e
Pavel Zharov <pavel@prna.ru>
2021-08-14 09:36:46 +0700

    fix: :ambulance: clear input fields when opening a profile popup

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -10,7 +10,7 @@ export default function EditProfilePopup ({ isOpen, onClose, onUpdateUser }) {
     useEffect(() => {
       setName(currentUser.name || '');
       setDescription(currentUser.about || '');
-    }, [currentUser]);
+    }, [currentUser, isOpen]);
 
     const handleChange = (e) => {
       e.target.name === 'name'
